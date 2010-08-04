43c5c75bfce010e897c089348919f86498c2f10d
thebretness <thebretness@gmail.com>
2010-08-03 21:21:41 -0500

    Created the default project in Android.

---
+++ src/com/thebretness/example/simple_calculator/CalculatorMain.java
@@ -0,0 +1,13 @@
+package com.thebretness.example.simple_calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculatorMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
