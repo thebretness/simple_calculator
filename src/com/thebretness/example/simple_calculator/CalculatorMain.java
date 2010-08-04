66f0e85d57e7f9243872593b7ae106f1787c5134
thebretness <thebretness@gmail.com>
2010-08-04 07:28:27 -0500

    Implemented the '7' button.

--- src/com/thebretness/example/simple_calculator/CalculatorMain.java
+++ src/com/thebretness/example/simple_calculator/CalculatorMain.java
@@ -2,6 +2,9 @@ package com.thebretness.example.simple_calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CalculatorMain extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,13 @@ public class CalculatorMain extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        final EditText text = (EditText) this.findViewById(R.id.entry);
+        
+        final Button button = (Button) findViewById(R.id.seven);
+        final String Rseven = this.getResources().getString(R.string.seven);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) { text.append(Rseven); }
+        });   
     }
 }
